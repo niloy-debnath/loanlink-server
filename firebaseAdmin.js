0d5bafbec9095d568ec1cc31ad5bf47ac345cbe6
niloy-debnath <niloydebnathofficial@gmail.com>
2025-12-22 22:41:16 +0600

    adminConfig restored

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,12 +1,8 @@
 import admin from "firebase-admin";
 
-if (!admin.apps.length) {
-  const serviceAccount = {
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  };
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
+if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
