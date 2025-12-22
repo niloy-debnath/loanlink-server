1bbc6a059f8369e986ba42c6f51d35f3f295a28d
niloy-debnath <niloydebnathofficial@gmail.com>
2025-12-22 21:14:45 +0600

    cors origin fixed to vercel origin

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.use(async (req, res, next) => {
 /* ===================== MIDDLEWARE ===================== */
 app.use(
   cors({
-    origin: true,
+    origin: "https://loanlink-server-one.vercel.app",
     credentials: true,
   })
 );
